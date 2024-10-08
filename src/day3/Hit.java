024f7b581f1005143f94f0c0de37b8b805b3a104
Beatrice <beatricesim31@gmail.com>
2024-10-08 13:12:49 +0800

    deleted unnecessary files

--- src/day3/Hit.java
+++
@@ -1,9 +0,0 @@
-package day3;
-
-public class Hit {
-    
-    private int hitPoints = 0;
-
-    public int getHitPoints() {return hitPoints;}
-    public void setHitPoints(int hitPoints) {this.hitPoints = hitPoints;}
-}
